a7d5666934b20e254c47d056781dece762b9b2e9
chandan <chandanppatel55@gmail.com>
2024-09-23 18:12:45 +0530

    New File Update

--- DOM/sc.js
+++ DOM/sc.js
@@ -1,14 +1,31 @@
-alert("Are you Sure");
-let header=document.getElementById("heading")// return value
-console.dir(header);
+// alert("Are you Sure");
+// let header=document.getElementById("heading")// return value
+// console.dir(header);
 
-let data=document.getElementsByClassName("con");// retunn html collections
-console.dir(data); 
+// let data=document.getElementsByClassName("con");// retunn html collections
+// console.dir(data); 
 
-let para=document.getElementsByTagName("p");
-console.dir(para);
-let all=document.querySelector("p");
-console.dir(all);
+// let para=document.getElementsByTagName("p");
+// console.dir(para);
+// let all=document.querySelector("p");
+// console.dir(all);
 
-let alls=document.querySelectorAll("p");
-console.dir(alls);
+// let alls=document.querySelectorAll("p");
+// console.dir(alls);
+
+// let buttons =document.querySelector("#Id");
+// console.dir(buttons);
+//  console.log(document.body.childNodes);
+
+ // textNode
+ //elements
+ //commets
+
+//  let div=document.querySelector("div");
+//  console.dir(div);
+//  let head=document.querySelector("h1");
+let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+" From Apna Collage Student!";
+
+ 
